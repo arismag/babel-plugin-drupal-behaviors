af0b9bf4a9dc1bc9fae95a53250f48639dfcbe5c
arismag <10897246+arismag@users.noreply.github.com>
2022-02-01 17:49:56 +0200

    Change behavior name to js filename of story in camel case format.

--- index.js
+++ index.js
@@ -2,34 +2,35 @@ const template = require('babel-template');
 
 const drupalBehavior = template(`Drupal.behaviors.NAME = {attach: function (context, settings) {BODY}};`);
 
-module.exports = function (babel) {
+module.exports = function(babel) {
   const t = babel.types;
 
-	return {
+  return {
     inherits: require("babel-plugin-transform-strict-mode"),
-		visitor: {
-			Program: {
-				exit (path) {
-					if (!this.drupalBehavior) {
+    visitor: {
+      Program: {
+        exit(path) {
+          if (!this.drupalBehavior) {
             this.drupalBehavior = true;
 
-            function getRandomNumber(max) {
-              return Math.floor(Math.random() * Math.floor(max));
+            var camelCase = function camelCase(str) {
+              return str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
             }
-            const randomNum = getRandomNumber(1000).toString();
 
-						const addBehavior = drupalBehavior({
-              NAME: t.identifier('name' + randomNum),
+            const storyName = camelCase(this.file.opts.sourceFileName.split('.').slice(0, -1).join('.'));
+
+            const addBehavior = drupalBehavior({
+              NAME: t.identifier(storyName),
               BODY: path.node.body
             });
 
-						path.replaceWith(
-							t.program([addBehavior])
-						);
-					}
-					path.node.directives = [];
-				}
-			}
-		}
-	};
+            path.replaceWith(
+              t.program([addBehavior])
+            );
+          }
+          path.node.directives = [];
+        }
+      }
+    }
+  };
 };
